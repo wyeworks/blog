68a69379a4769a39a278084f928ac2941184b216
Santiago Pastorino <santiago@wyeworks.com>
2013-01-06 15:17:38 -0200

    Add chruby to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,14 @@
 require 'capistrano/ext/multistage'
 require "bundler/capistrano"
 
+default_run_options[:shell] = '/bin/bash'
+set :ruby_version, "1.9.3"
+set :chruby_script, "/usr/local/share/chruby/chruby.sh"
+set :set_ruby_cmd, ". #{chruby_script} && chruby #{ruby_version}"
+set(:bundle_cmd) {
+  "#{set_ruby_cmd} && exec bundle"
+}
+
 set :stages, %w(staging production)
 set :default_stage, 'production'
 
