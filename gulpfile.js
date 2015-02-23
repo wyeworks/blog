ffddd421b8991c46d627b5b865225f0d2fcd751d
DrummerHead <drummerhead@gmail.com>
2015-02-23 05:20:24 -0200

    Change root of blog from '/' to '/blog'

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,15 @@ var htmlmin = require('gulp-html-minifier');
 var uglify = require('gulp-uglify');
 
 gulp.task('minify-html', function(){
-  gulp.src('./public/**/*.html')
+  gulp.src('./public/blog/**/*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('./public'));
+    .pipe(gulp.dest('./public/blog'));
 });
 
 gulp.task('minify-js', function(){
-  gulp.src('./public/javascripts/octopress.js')
+  gulp.src('./public/blog/javascripts/octopress.js')
     .pipe(uglify())
-    .pipe(gulp.dest('./public/javascripts/'));
+    .pipe(gulp.dest('./public/blog/javascripts/'));
 });
 
 gulp.task('default', ['minify-html', 'minify-js']);
