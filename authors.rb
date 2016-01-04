b58d31d52d2b3a3e1c3243b2e84e9b07337fed3b
Santiago Pastorino <santiago@wyeworks.com>
2016-01-04 12:33:02 -0300

    Reorder authors alphabetically

--- authors.rb
+++ authors.rb
@@ -1,11 +1,19 @@
 @authors = {
-  undefined_author: {
-    name: '',
-    email: '',
-    twitter_handle: '',
-    github_handle: '',
-    image: '',
-    description: ''
+  alexis_mas: {
+    name: 'Alexis Mas',
+    email: 'alexis@wyeworks.com',
+    twitter_handle: '_axxiss',
+    github_handle: 'axxiss',
+    image: 'alexis-mas.jpg',
+    description: 'Software Engineer, turning coffee into software since 2008'
+  },
+  marcelo_dominguez: {
+    name: 'Marcelo Dominguez',
+    email: 'marcelo@wyeworks.com',
+    twitter_handle: 'marpo60',
+    github_handle: 'marpo60',
+    image: 'marcelo-dominguez.jpg',
+    description: 'Software engineer - Curious by nature - Sports lover - Tech guy'
   },
   jorge_bejar: {
     name: 'Jorge Bejar',
@@ -23,6 +31,22 @@
     image: 'jose-ignacio-costa.jpg',
     description: 'Cofounder & CEO at WyeWorks. Fitness aficionado. Home Barista wannabe.'
   },
+  juan_azambuja: {
+    name: 'Juan Azambuja',
+    email: 'juan@wyeworks.com',
+    twitter_handle: 'juanazam',
+    github_handle: 'juanazam',
+    image: 'juan-azambuja.jpg',
+    description: 'Passionate Rails developer now transitioning to Ember'
+  },
+  rodrigo_ponce_de_leon: {
+    name: 'Rodrigo Ponce de León',
+    email: 'rodrigo@wyeworks.com',
+    twitter_handle: 'rponce_89',
+    github_handle: 'rodrigopdl',
+    image: 'rodrigo-pdl.jpg',
+    description: 'Ruby on Rails developer at WyeWorks. Beatlemaniac. Guitar freak.'
+  },
   santiago_ferreira: {
     name: 'Santiago Ferreira',
     email: 'santiago.ferreira@wyeworks.com',
@@ -30,14 +54,6 @@
     github_handle: 'san650',
     image: 'santiago-ferreira.jpg',
     description: ''
-  },
-  sebastian_martinez: {
-    name: 'Sebastián Martínez',
-    email: 'sebastian@wyeworks.com',
-    twitter_handle: 'smartinez87',
-    github_handle: 'smartinez87',
-    image: 'sebastian-martinez.jpg',
-    description: 'VP of Engineering at WyeWorks. Ruby on Rails hacker. ExceptionNotification maintainer. Coffee & bacon lover.'
   },
   santiago_pastorino: {
     name: 'Santiago Pastorino',
@@ -47,36 +63,20 @@
     image: 'santiago-pastorino.jpg',
     description: 'WyeWorks Co-Founder & CTO, Ruby on Rails Core Team Member'
   },
-  marcelo_dominguez: {
-    name: 'Marcelo Dominguez',
-    email: 'marcelo@wyeworks.com',
-    twitter_handle: 'marpo60',
-    github_handle: 'marpo60',
-    image: 'marcelo-dominguez.jpg',
-    description: 'Software engineer - Curious by nature - Sports lover - Tech guy'
+  sebastian_martinez: {
+    name: 'Sebastián Martínez',
+    email: 'sebastian@wyeworks.com',
+    twitter_handle: 'smartinez87',
+    github_handle: 'smartinez87',
+    image: 'sebastian-martinez.jpg',
+    description: 'VP of Engineering at WyeWorks. Ruby on Rails hacker. ExceptionNotification maintainer. Coffee & bacon lover.'
   },
-  juan_azambuja: {
-    name: 'Juan Azambuja',
-    email: 'juan@wyeworks.com',
-    twitter_handle: 'juanazam',
-    github_handle: 'juanazam',
-    image: 'juan-azambuja.jpg',
-    description: 'Passionate Rails developer now transitioning to Ember'
-  },
-  alexis_mas: {
-    name: 'Alexis Mas',
-    email: 'alexis@wyeworks.com',
-    twitter_handle: '_axxiss',
-    github_handle: 'axxiss',
-    image: 'alexis-mas.jpg',
-    description: 'Software Engineer, turning coffee into software since 2008'
-  },
-  rodrigo_ponce_de_leon: {
-    name: 'Rodrigo Ponce de León',
-    email: 'rodrigo@wyeworks.com',
-    twitter_handle: 'rponce_89',
-    github_handle: 'rodrigopdl',
-    image: 'rodrigo-pdl.jpg',
-    description: 'Ruby on Rails developer at WyeWorks. Beatlemaniac. Guitar freak.'
+  undefined_author: {
+    name: '',
+    email: '',
+    twitter_handle: '',
+    github_handle: '',
+    image: '',
+    description: ''
   }
 }
